829dc82a0350a4b114da72fc5c3ee726dfa56546
Björn Albers <bjoernalbers@googlemail.com>
2012-08-23 15:19:04 +0200

    Improve fact $has_compiler
    On my test system was the file '/usr/bin/scrun' present but no command
    line tools installed.

--- lib/facter/has_compiler.rb
+++ lib/facter/has_compiler.rb
@@ -1,8 +1,6 @@
 Facter.add(:has_compiler) do
   confine :operatingsystem => :darwin
   setcode do
-    macosx_version = Facter.value('macosx_productversion_major').to_f
-    compiler = (macosx_version < 10.7 && '/usr/bin/cc') || '/usr/bin/xcrun'
-    File.exists?(compiler)
+    File.exists?('/usr/bin/cc') || system('/usr/bin/xcrun -find cc >/dev/null 2>&1')
   end
 end
