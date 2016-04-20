67230fd4e38a3ff1af27cdef079c19798990ba05
Kevin James <KevinJames@thekev.in>
2016-04-19 18:00:18 -0700

    fix typo: warn -> warning

--- manifests/compiler.pp
+++ manifests/compiler.pp
@@ -2,7 +2,7 @@ class homebrew::compiler {
 
   if $::has_compiler == true {
   } elsif versioncmp($::macosx_productversion_major, '10.7') < 0 {
-    warn('Please install the Command Line Tools bundled with XCode manually!')
+    warning('Please install the Command Line Tools bundled with XCode manually!')
   } else {
 
     if ($homebrew::command_line_tools_package and $homebrew::command_line_tools_source) {
@@ -16,7 +16,7 @@ class homebrew::compiler {
       }
 
     } else {
-      warn('No Command Line Tools detected and no download source set. If Command Line Tools is installed, this may be a false positive. If not, please set download sources or install manually.')
+      warning('No Command Line Tools detected and no download source set. If Command Line Tools is installed, this may be a false positive. If not, please set download sources or install manually.')
     }
 
   }
