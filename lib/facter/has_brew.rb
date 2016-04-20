5a3ef8a8f2d02717a3052326194ba71989706bcd
Kevin James <KevinJames@thekev.in>
2016-04-19 18:45:23 -0700

    fix OS detection in facts

--- lib/facter/has_brew.rb
+++ lib/facter/has_brew.rb
@@ -1,4 +1,11 @@
 Facter.add(:has_brew) do
+  setcode do
+    warn('Unsupported OS.')
+    nil
+  end
+end
+
+Facter.add(:has_brew) do
   confine :operatingsystem => :darwin
   setcode do
     File.exists?('/usr/local/bin/brew') || system('brew --version >/dev/null 2>&1')
