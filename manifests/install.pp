d5f87eff050de5338b43f5989adc2a9fa4e38f6c
Kevin James <kevin@atomicmgmt.com>
2016-08-31 10:57:19 -0700

    fix #44 - set more fine-grained permissions in /usr/local

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,8 +4,27 @@ class homebrew::install {
     ensure  => directory,
     owner   => $homebrew::user,
     group   => $homebrew::group,
-    mode    => '0775',
+    mode    => '0644',
     recurse => true,
+  } ->
+  file { '/usr/local/Library': ensure => directory } ->
+  file { '/usr/local/Library/Homebrew': ensure => directory } ->
+  file { '/usr/local/Library/Homebrew/cask': ensure => directory }
+  file { '/usr/local/Library/Homebrew/shims':
+    ensure  => directory,
+    require => File['/usr/local/Library/Homebrew'],
+  }
+
+  file { ['/usr/local/Library/Homebrew/cask/cmd',
+          '/usr/local/Library/Homebrew/shims/scm',
+          '/usr/local/Library/Homebrew/shims/super',
+          '/usr/local/bin',
+          '/usr/local/Cellar']:
+    ensure  => directory,
+    mode    => '0755',
+    recurse => true,
+    require => [File['/usr/local/Library/Homebrew/cask'],
+                File['/usr/local/Library/Homebrew/shims']],
   }
 
   exec { 'install-homebrew':
