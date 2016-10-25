942f58783df40101bd6bdfae0f2bf5074d38e54c
Kevin James <KevinJames@thekev.in>
2016-10-25 13:34:34 -0700

    fix #44 - only manage permissions for /usr/local/Homebrew (#58)

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,20 +1,21 @@
 class homebrew::install {
 
-  file { ['/usr/local', '/Library/Caches/Homebrew']:
+  file { '/usr/local':
+    ensure  => directory,
+  } ->
+  file { '/usr/local/Homebrew':
     ensure  => directory,
     owner   => $homebrew::user,
     group   => $homebrew::group,
     mode    => '0775',
     recurse => true,
-  }
-
+  } ->
   exec { 'install-homebrew':
     cwd       => '/usr/local',
     command   => "/usr/bin/su ${homebrew::user} -c '/bin/bash -o pipefail -c \"/usr/bin/curl -skSfL https://github.com/homebrew/brew/tarball/master | /usr/bin/tar xz -m --strip 1\"'",
     creates   => '/usr/local/bin/brew',
     logoutput => on_failure,
     timeout   => 0,
-    require   => File['/usr/local'],
   } ~>
   file { '/usr/local/bin/brew':
     owner => $homebrew::user,
