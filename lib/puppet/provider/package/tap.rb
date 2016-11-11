3a760bc7750c54478f3dc0201d8f91d0811056f2
Jordi Garcia <jordi@improbable.io>
2016-11-11 18:37:05 +0000

    Moving to match array as suggested

--- lib/puppet/provider/package/tap.rb
+++ lib/puppet/provider/package/tap.rb
@@ -37,7 +37,7 @@ Puppet::Type.type(:package).provide(:tap,
       output = execute([command(:brew), :tap])
       output.each_line do |line|
         line.chomp!
-        return { :name => line, :ensure => 'present', :provider => 'tap' }  if line.downcase == name || line.downcase == name.gsub('homebrew-', '')
+        return { :name => line, :ensure => 'present', :provider => 'tap' } if [name, name.gsub('homebrew-', '')].include? line.downcase?
       end
     rescue Puppet::ExecutionFailure => detail
       Puppet.Err "Could not query tap: #{detail}"
