26e64f24c9ea1e5ec1f692d40a1ed46acc43b604
Jordi Garcia <jordigg@users.noreply.github.com>
2016-11-12 18:59:57 +0000

    Fix homebrew install (#64)

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,26 +1,45 @@
 class homebrew::install {
 
-  file { '/usr/local':
-    ensure  => directory,
+  $brew_folders = [
+                    '/usr/local',
+                    '/usr/local/bin',
+                    '/usr/local/Cellar',
+                    '/usr/local/etc',
+                    '/usr/local/Frameworks',
+                    '/usr/local/include',
+                    '/usr/local/lib',
+                    '/usr/local/lib/pkgconfig',
+                    '/usr/local/opt',
+                    '/usr/local/share',
+                    '/usr/local/share/doc',
+                    '/usr/local/share/man',
+                    '/usr/local/var',
+                  ]
+
+  file { $brew_folders:
+    ensure => directory,
+    group  => $homebrew::group,
+    mode   => '0775',
   } ->
   file { '/usr/local/Homebrew':
-    ensure  => directory,
-    owner   => $homebrew::user,
-    group   => $homebrew::group,
-    mode    => '0775',
-    recurse => true,
+    ensure => directory,
+    owner  => $homebrew::user,
+    group  => $homebrew::group,
+    mode   => '0775',
   } ->
   exec { 'install-homebrew':
-    cwd       => '/usr/local',
+    cwd       => '/usr/local/Homebrew',
     command   => "/usr/bin/su ${homebrew::user} -c '/bin/bash -o pipefail -c \"/usr/bin/curl -skSfL https://github.com/homebrew/brew/tarball/master | /usr/bin/tar xz -m --strip 1\"'",
-    creates   => '/usr/local/bin/brew',
+    creates   => '/usr/local/Homebrew/bin/brew',
     logoutput => on_failure,
     timeout   => 0,
   } ~>
   file { '/usr/local/bin/brew':
-    owner => $homebrew::user,
-    group => $homebrew::group,
-    mode  => '0775',
+    ensure => 'link',
+    target => '/usr/local/Homebrew/bin/brew',
+    owner  => $homebrew::user,
+    group  => $homebrew::group,
+    mode   => '0775',
   }
 
 }
