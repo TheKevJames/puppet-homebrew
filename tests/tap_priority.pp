ae0349a316def9d3ee72d5094eff9a145e138bb9
Kevin James <KevinJames@thekev.in>
2016-12-14 18:11:32 -0800

    break tap priority test

--- tests/tap_priority.pp
+++ tests/tap_priority.pp
@@ -1,10 +1,7 @@
-Package <| provider == tap |> -> Package <| provider == homebrew |>
-
 package { 'homebrew/science':
   ensure   => present,
   provider => tap,
-}
-
+} ->
 package { 'bact':
   ensure   => present,
   provider => homebrew,
