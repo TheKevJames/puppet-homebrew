162404a5c40010382f90edf045699edc17a423d8
Chris Edester <edestecd@miamioh.edu>
2017-01-04 21:57:19 -0500

    Update facts to work in puppet 3 or 4

--- lib/facter/has_compiler.rb
+++ lib/facter/has_compiler.rb
@@ -1,21 +1,29 @@
-Facter.add(:has_compiler) do
-  setcode do
-    "nil"
-  end
-end
+# Fact: has_compiler
+#
+# Purpose: check if Xcode Command Line Tools is installed
+#
+# Resolution:
+#   Tests for presence of cc, returns boolean
+#   No value set if not on Darwin
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
 
 Facter.add(:has_compiler) do
-  confine :operatingsystem => :darwin
+  confine :operatingsystem => 'Darwin'
   setcode do
-    File.exists?('/usr/bin/cc') or system('/usr/bin/xcrun -find cc >/dev/null 2>&1') ? "true" : "false"
+    File.exists?('/usr/bin/cc') or system('/usr/bin/xcrun -find cc >/dev/null 2>&1')
   end
 end
 
 Facter.add(:has_compiler) do
   # /usr/bin/cc exists in Mavericks, but it's not real
-  confine :operatingsystem => :darwin, :macosx_productversion_major => '10.9'
+  confine :operatingsystem => 'Darwin', :macosx_productversion_major => '10.9'
   setcode do
     (File.exists?('/Applications/Xcode.app') or File.exists?('/Library/Developer/CommandLineTools/')) and
-        (File.exists?('/usr/bin/cc') or system('/usr/bin/xcrun -find cc >/dev/null 2>&1')) ? "true" : "false"
+        (File.exists?('/usr/bin/cc') or system('/usr/bin/xcrun -find cc >/dev/null 2>&1'))
   end
 end
