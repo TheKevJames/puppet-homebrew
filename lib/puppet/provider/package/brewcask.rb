b1bb9bd60246e7db3db00e46eeabbbe01429f1d0
Kevin James <KevinJames@thekev.in>
2026-02-25 13:43:04 +0000

    refactor: dedupe common provider code

--- lib/puppet/provider/package/brewcask.rb
+++ lib/puppet/provider/package/brewcask.rb
@@ -1,10 +1,12 @@
-require 'etc'
 require 'puppet/provider/package'
+require 'puppet/provider/package/homebrew_common'
 
 Puppet::Type.type(:package).provide(:brewcask, parent: Puppet::Provider::Package) do
   desc 'Package management using HomeBrew casks on OSX'
 
   confine operatingsystem: :darwin
+
+  include Puppet::Provider::Package::HomebrewCommon
 
   has_feature :installable
   has_feature :uninstallable
@@ -12,84 +14,11 @@ Puppet::Type.type(:package).provide(:brewcask, parent: Puppet::Provider::Package
   has_feature :versionable
   has_feature :install_options
 
-  def self.brewbin
-    @brewbin ||= ['/opt/homebrew/bin/brew', '/usr/local/bin/brew'].find { |path| File.exist?(path) }
-  end
-
   commands brew: brewbin
   commands stat: '/usr/bin/stat'
 
-  def self.with_unbundled_env
-    return yield unless Puppet.features.bundled_environment?
-
-    if Bundler.respond_to?(:with_unbundled_env)
-      Bundler.with_unbundled_env { yield }
-    else
-      Bundler.with_clean_env { yield }
-    end
-  end
-
-  def self.execute(cmd, failonfail = false, combine = false)
-    owner = stat('-nf', '%Uu', brewbin).to_i
-    group = stat('-nf', '%Ug', brewbin).to_i
-    home = Etc.getpwuid(owner).dir
-
-    if owner.zero?
-      raise Puppet::ExecutionFailure,
-            'Homebrew does not support installations owned by the "root" user. Please check the permissions of /usr/local/bin/brew'
-    end
-
-    uid, gid = if Process.uid.zero?
-                 [owner, group]
-               else
-                 [nil, nil]
-               end
-
-    with_unbundled_env do
-      super(cmd,
-            uid: uid,
-            gid: gid,
-            combine: combine,
-            custom_environment: { 'HOME' => home },
-            failonfail: failonfail)
-    end
-  end
-
   def self.instances
     package_list.map { |hash| new(hash) }
-  end
-
-  def execute(*args)
-    self.class.execute(*args)
-  end
-
-  def fix_checksum(files)
-    files.each { |file| File.delete(file) }
-  rescue Errno::ENOENT
-    Puppet.warning("Could not remove mismatched checksum files #{files}")
-  ensure
-    raise Puppet::ExecutionFailure, "Checksum error for package #{name} in files #{files}"
-  end
-
-  def resource_name
-    return resource[:name] if resource[:name].match?(%r{^https?://})
-
-    resource[:name].downcase
-  end
-
-  def install_name
-    should = resource[:ensure].downcase
-
-    case should
-    when true, false, Symbol
-      resource_name
-    else
-      "#{resource_name}@#{should}"
-    end
-  end
-
-  def install_options
-    Array(resource[:install_options]).flatten.compact
   end
 
   def latest
--- lib/puppet/provider/package/tap.rb
+++ lib/puppet/provider/package/tap.rb
@@ -1,68 +1,21 @@
-require 'etc'
 require 'puppet/provider/package'
+require 'puppet/provider/package/homebrew_common'
 
 Puppet::Type.type(:package).provide(:tap, parent: Puppet::Provider::Package) do
   desc 'Tap management using HomeBrew on OSX'
 
   confine operatingsystem: :darwin
 
+  include Puppet::Provider::Package::HomebrewCommon
+
   has_feature :installable
   has_feature :uninstallable
   has_feature :install_options
 
-  def self.brewbin
-    @brewbin ||= ['/opt/homebrew/bin/brew', '/usr/local/bin/brew'].find { |path| File.exist?(path) }
-  end
-
   commands brew: brewbin
   commands stat: '/usr/bin/stat'
 
-  def self.with_unbundled_env
-    return yield unless Puppet.features.bundled_environment?
-
-    if Bundler.respond_to?(:with_unbundled_env)
-      Bundler.with_unbundled_env { yield }
-    else
-      Bundler.with_clean_env { yield }
-    end
-  end
-
-  def self.execute(cmd, failonfail = false, combine = false)
-    owner = stat('-nf', '%Uu', brewbin).to_i
-    group = stat('-nf', '%Ug', brewbin).to_i
-    home = Etc.getpwuid(owner).dir
-
-    if owner.zero?
-      raise Puppet::ExecutionFailure,
-            'Homebrew does not support installations owned by the "root" user. Please check the permissions of /usr/local/bin/brew'
-    end
-
-    uid, gid = if Process.uid.zero?
-                 [owner, group]
-               else
-                 [nil, nil]
-               end
-
-    with_unbundled_env do
-      super(cmd,
-            uid: uid,
-            gid: gid,
-            combine: combine,
-            custom_environment: { 'HOME' => home },
-            failonfail: failonfail)
-    end
-  end
-
-  def execute(*args)
-    self.class.execute(*args)
-  end
-
-  def install_options
-    Array(resource[:install_options]).flatten.compact
-  end
-
   def install
-    resource_name = resource[:name].downcase
     Puppet.debug("Tapping #{resource_name}")
     execute([command(:brew), :tap, resource_name, *install_options], failonfail: true)
   rescue Puppet::ExecutionFailure => detail
@@ -70,7 +23,6 @@ Puppet::Type.type(:package).provide(:tap, parent: Puppet::Provider::Package) do
   end
 
   def uninstall
-    resource_name = resource[:name].downcase
     Puppet.debug("Untapping #{resource_name}")
     execute([command(:brew), :untap, resource_name], failonfail: true)
   rescue Puppet::ExecutionFailure => detail
@@ -78,8 +30,6 @@ Puppet::Type.type(:package).provide(:tap, parent: Puppet::Provider::Package) do
   end
 
   def query
-    resource_name = resource[:name].downcase
-
     Puppet.debug("Querying tap #{resource_name}")
     output = execute([command(:brew), :tap])
     output.each_line do |line|
